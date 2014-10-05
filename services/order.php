f77205e1e849f7b8154c6596be50ba9f81e05eef
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-10-05 22:31:19 +0100

    DB used for the orders

--- services/order.php
+++ services/order.php
@@ -2,12 +2,19 @@
 
 namespace services;
 
-require_once "Service.php";
+require_once "services/Service.php";
+require_once "entities/Order.php";
+require_once "models/Order/Sqlite.php";
 
 class order extends Service
 {
+	public function __construct()
+	{
+		\entities\Order::setModel(new \models\Order\Sqlite());
+	}
+
 	public function get(array $criterias = array())
 	{
-		var_dump($criterias);
+		return \entities\Order::getOrders($criterias);
 	}
 }
