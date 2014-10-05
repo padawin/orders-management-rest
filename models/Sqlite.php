54c5417ab4f25b0ce4195533919aaefe038ce21d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-10-05 23:28:14 +0100

    methods put and post added in the model

--- models/Sqlite.php
+++ models/Sqlite.php
@@ -40,6 +40,40 @@ class Sqlite implements Model
 		return $stmt->fetchAll();
 	}
 
+	public function update(array $values, array $criterias = array())
+	{
+		$sql = "UPDATE " . static::$_table . " SET ";
+		$params = $fields = $where = array();
+		foreach ($values as $col => $val) {
+			$fields[] = $col . ' = :col_' . $col;
+			$params['col_' . $col] = $val;
+		}
+		foreach ($criterias as $col => $val) {
+			$where[] = $col . ' = :where_' . $col;
+			$params['where_' . $col] = $val;
+		}
+
+		$sql .= implode(', ', $fields)
+		. (!empty($where) ? " WHERE " . implode(" AND ", $where) : '');
+
+		return $this->_execute($sql, $params)[1];
+	}
+
+	public function insert(array $values)
+	{
+		if (empty($values)) {
+			throw new \exceptions\BadRequest;
+		}
+
+		$sql = sprintf(
+			"INSERT INTO " . static::$_table . " (%s) VALUES (%s)",
+			implode(', ', array_keys($values)),
+			implode(', ', array_fill(0, count($values), '?'))
+		);
+
+		return $this->_execute($sql, array_values($values))[1];
+	}
+
 	protected function _execute($sql, $params = array())
 	{
 		try {
