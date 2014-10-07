aa5c3953c72e9bba002d5db9c7670efff5e9e08d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-10-07 14:23:11 +0100

    hardcoded value changed

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 require_once "Registry.php";
 
 $config = array(
-	'root' => '/home/ghislain/projets/workspace/php/order-management',
+	'root' => '/path/to/the/project/root',
 	'default-vat' => .2
 );
 
