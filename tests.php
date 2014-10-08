993ccf5f4d0b56096651201f4145aa14741a0865
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-10-08 08:45:48 +0100

    tests class implemented with the products tests

--- tests.php
+++ tests.php
@@ -14,19 +14,61 @@ use \Services\Order as OrderService;
 use \Services\Product as ProductService;
 use \Services\LineItem as LineItemService;
 
-$orderService = new OrderService();
-$productService = new ProductService();
-$lineItemService = new LineItemService();
+class Tests
+{
+	public function setup()
+	{
+		if (is_file(Registry::get('db'))) {
+			unlink(Registry::get('db'));
+			exec("./INSTALL " . 'orders-tests.db');
+		}
+	}
 
-$orders = $orderService->post(array());
+	// Users need to be able to maintain a list of products
+	public function test1()
+	{
+		$productService = new ProductService();
+		try {
+			echo "Creation of an empty product should fail: ";
+			$productService->post();
+			echo "KO\n";
+		}
+		catch (\Exceptions\BadRequest $e) {
+			echo "OK\n";
+		}
 
-try {
-	$productService->post();
+		$myProduct = $productService->post(array("name" => "my fist product", "price" => 20));
+		echo "The newly created product id should be 1: ",
+			$myProduct == 1 ? "OK" : "KO", "\n";
+
+		$myProduct = $productService->get(array("id_product" => 1));
+		echo "The product's name should be \"my fist product\": ",
+			$myProduct[0]['name'] == "my fist product" ? 'OK' : 'KO', "\n";
+
+		$updatedRows = $productService->put(array("name" => "new name"), array("id_product" => 1));
+		echo "1 Row should have been edited: ",
+			$updatedRows == 1 ? "OK" : "KO", "\n";
+
+		$nbDeleted = $productService->delete(array("id_product" => 1));
+		echo "1 Row should have been deleted: ",
+			$nbDeleted == 1 ? "OK" : "KO", "\n";
+
+		$myProduct = $productService->get(array("id_product" => 1));
+		echo "Np product is supposed to be found: ",
+			empty($myProduct) ? 'OK' : 'KO', "\n";
+	}
+
+	public function run()
+	{
+		$methods = get_class_methods($this);
+		foreach ($methods as $method) {
+			if (strpos($method, 'test') === 0) {
+				$this->setup();
+				$this->$method();
+			}
+		}
+	}
 }
-catch (\Exceptions\BadRequest $e) {
-	var_dump("expected");
-}
-var_dump($orders);
 
-$orders = $orderService->get(array());
-var_dump($orders);
+$tests = new Tests();
+$tests->run();
